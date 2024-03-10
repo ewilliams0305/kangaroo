9a1aa6f09963061d87eaa7b7c88b7783a961d2a3
Eric Williams <ewilliams0305@gmail.com>
2024-03-10 08:57:17 -0400

    created basic query tests

---
+++ tests/Queries/OrderlyPing_Query_Tests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Kangaroo.Queries;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Kangaroo.UnitTests.Queries;
+// ReSharper disable once InconsistentNaming
+
+public class OrderlyPing_Query_Tests
+{
+
+    [Fact]
+    public async Task Query_Returns_IpStatusSuccess_WhenIpIsValid()
+    {
+        // Arrange
+        using var pingQuery = new QueryPingResultsOrderly(new DefaultLogger(), new Ping(),
+            new QueryOptions(1, TimeSpan.FromSeconds(1)));
+
+        // Act
+        var ping = await pingQuery.Query(IPAddress.Loopback);
+
+        // Assert
+
+        ping!.Status.Should().Be(IPStatus.Success);
+    }
+    
+    [Fact]
+    public async Task Query_Returns_IpStatusFailure_WhenIpIsInvalid()
+    {
+        // Arrange
+        using var pingQuery = new QueryPingResultsOrderly(new DefaultLogger(), new Ping(),
+            new QueryOptions(1, TimeSpan.FromSeconds(1)));
+
+        // Act
+        var ping = await pingQuery.Query(IPAddress.Parse("192.168.55.237"));
+
+        // Assert
+
+        ping!.Status.Should().NotBe(IPStatus.Success);
+    }
+
+}
---
+++ tests/Queries/ParallelPing_Query_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using FluentAssertions;
+using Kangaroo.Queries;
+
+namespace Kangaroo.UnitTests.Queries;
+
+// ReSharper disable once InconsistentNaming
+public class ParallelPing_Query_Tests
+{
+
+    [Fact]
+    public async Task Query_Returns_IpStatusSuccess_WhenIpIsValid()
+    {
+        // Arrange
+        using var pingQuery = new QueryPingResultsParallel(new DefaultLogger(), new QueryOptions(1, TimeSpan.FromSeconds(1)));
+
+        // Act
+        var ping = await pingQuery.Query(IPAddress.Loopback);
+
+        // Assert
+
+        ping!.Status.Should().Be(IPStatus.Success);
+    }
+    
+    [Fact]
+    public async Task Query_Returns_IpStatusFailure_WhenIpIsInvalid()
+    {
+        // Arrange
+        using var pingQuery = new QueryPingResultsParallel(new DefaultLogger(), new QueryOptions(1, TimeSpan.FromSeconds(1)));
+
+        // Act
+        var ping = await pingQuery.Query(IPAddress.Parse("192.168.55.237"));
+
+        // Assert
+
+        ping!.Status.Should().NotBe(IPStatus.Success);
+    }
+
+}
